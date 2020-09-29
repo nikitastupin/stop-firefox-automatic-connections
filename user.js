4644d5998d99f3f361b863c72e4ecd3144fca7a2
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2020-09-29 21:34:35 +0300

    Disable telemetry (e.g. incoming.telemetry.mozilla.org)

--- user.js
+++ user.js
@@ -22,3 +22,20 @@ user_pref("network.captive-portal-service.enabled", false);
 user_pref("dom.push.connection.enabled", false);
 
 user_pref("browser.search.suggest.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+
+// https://gist.github.com/gamunu/7fbee4e2318fdc080395a7f74cc34fe9
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.server", "");
