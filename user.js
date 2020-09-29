105e6a28ff7acb42d9d04e15b25aa08bab0cc8d4
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2020-09-29 11:06:48 +0300

    Add preferences from http://mzl.la/1DFDuS2

---
+++ user.js
@@ -0,0 +1,17 @@
+user_pref("extensions.blocklist.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+user_pref("network.prefetch-next", false);
+user_pref("network.dns.disablePrefetch", false);
+user_pref("network.http.speculative-parallel-limit", false);
+
+user_pref("browser.aboutHomeSnippets.updateUrl", false);
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("extensions.getAddons.cache.enabled", false);
+
+user_pref("app.normandy.enabled", false);
+
+user_pref("media.gmp-gmpopenh264.enabled", false);
+
+user_pref("network.captive-portal-service.enabled", false);
