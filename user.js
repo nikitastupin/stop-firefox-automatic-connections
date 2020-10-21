7ecf68104d0970d8bf85c119ec42a7fe2c563aac
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2020-10-21 11:07:28 +0300

    Remove captive portal URL

--- user.js
+++ user.js
@@ -18,6 +18,7 @@ user_pref("app.normandy.enabled", false);
 user_pref("media.gmp-gmpopenh264.enabled", false);
 
 user_pref("network.captive-portal-service.enabled", false);
+user_pref("captivedetect.canonicalURL", "");
 
 user_pref("dom.push.connection.enabled", false);
 
